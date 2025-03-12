abbef23448f4697eace8cc09d944fe0c1f42079e
Temur Coder <temurnarzullayev810@gmail.com>
2025-03-12 17:49:52 +0300

    added code

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-let container = document.querySelector('.container');
+let container = document.querySelector(".container");
 
 const tech = fetch("https://jsonplaceholder.typicode.com/photos")
-.then(response => {
+  .then((response) => {
     return response.json();
-})
+  })
+  .then((data) => {
+    data.forEach((elemn) => {
+      const element = document.createElement("div");
+      element.classList.add("post");
+      element.innerHTML = `
+      
+      
+      
+      `;
+      container.appendChild(element);
+    });
+  });
 console.log(tech);
