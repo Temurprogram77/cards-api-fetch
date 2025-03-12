f9b35ba98ced7ac16d9f6bed6f7b7798ce6dcc58
Temur Coder <temurnarzullayev810@gmail.com>
2025-03-12 17:50:19 +0300

    new card created js

--- script.js
+++ script.js
@@ -10,7 +10,14 @@ const tech = fetch("https://jsonplaceholder.typicode.com/photos")
       element.classList.add("post");
       element.innerHTML = `
       
-      
+        <div class="card">
+            <img src="/index.html" alt="photo">
+            <p class="title">Accusamus beatae ad facilis cum similique qui sunt</p>
+            <div class="ids">
+                <div class="book_id">2</div>
+                <div class="id">2</div>
+            </div>
+        </div>
       
       `;
       container.appendChild(element);
